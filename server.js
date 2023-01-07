7e3578b500f3877f9262437d3447e272a12315dd
J0J0 <iditaroddream@gmail.com>
2023-01-07 07:11:56 -0600

    update

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const {v4:uuidv4} = require('uuid');
+const path = require('path');
+const { readAndAppend, readFromFile, writeToFile } = require('./helpers/fsUtils');
+const { receiveMessageOnPort } = require('worker_threads');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+readFromFile('./db/db.json','utf-8')
+.then(data =>res.json(JSON.parse(data)))
+});
+
+app.post('/api/notes', (req, res) => {
+  readAndAppend({
+    id:uuidv4(),
+    ...req.body 
+  },'./db/db.json')
+  .then(data =>{
+    res.json(req.body)
+  })
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  readFromFile('./db/db.json','utf-8')
+  .then(data =>{
+const notes = JSON.parse(data).filter(note => (
+  note.id !== req.params.id
+))
+  return writeToFile('./db/db.json', notes)
+  }).then(()=>res.json({
+    success:true
+  }))
+});
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
